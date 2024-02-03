3566035e65f21d2be62d3829c240c0279e14a2fb
Neskech <neskechastro@gmail.com>
2024-02-03 13:39:09 -0500

    woooo

--- src/components/DisplayCard.tsx
+++ src/components/DisplayCard.tsx
@@ -1,17 +1,32 @@
 import React from "react";
-import { Card, CardBody } from "react-bootstrap";
+import { Button, Card, CardBody } from "react-bootstrap";
 
 type DisplayCardProps = {
   title: string;
   text: string;
+  isSelected: boolean;
+  onTextChange: (newText: string) => void;
+  onGPTGenerate: () => void;
+  onDelete: () => void;
 };
 
-export default function DisplayCard({ title, text }: DisplayCardProps) {
+export default function DisplayCard(props: DisplayCardProps) {
   return (
     <Card className="border-10 relative top-[0vh] rounded-md">
       <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text className="text-pretty">{text}</Card.Text>
+        <Card.Title>{props.title}</Card.Title>
+        <div className="flex flex-row items-center justify-center">
+          <Button className="mr-auto" onClick={(_) => props.onDelete()}>
+            Delete
+          </Button>
+
+          {props.text.length === 0 && (
+            <Button className="mr-auto" onClick={(_) => props.onGPTGenerate()}>
+              AI Fill
+            </Button>
+          )}
+        </div>
+        <Card.Text className="text-pretty">{props.text}</Card.Text>
       </Card.Body>
     </Card>
   );
